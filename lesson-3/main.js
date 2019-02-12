c4adc52349261eb5176d3e8c34c990a7ed11c4b4
VadimShy <vadim.s.yu@gmail.com>
2019-02-12 13:35:30 +0200

    Several conditions: “else if”

--- lesson-3/main.js
+++ lesson-3/main.js
@@ -1,7 +1,9 @@
 var year = prompt('What is your birth year?', '');
-if(year != 1990) {
-    alert('Wrong!');
-    alert('Try again');
+if(year < 1990) {
+    alert('Too early...');
+}
+else if(year > 1990) {
+    alert('Too late');
 }
 else {
     alert('Yeap');
