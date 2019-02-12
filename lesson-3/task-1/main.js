4dde4f1235dcf7d4205b38d015bc5d06e611f67e
VadimShy <vadim.s.yu@gmail.com>
2019-02-12 14:50:13 +0200

    The name of JavaScript

---
+++ lesson-3/task-1/main.js
@@ -0,0 +1,7 @@
+var name = prompt('What is the “official” name of JavaScript?', '');
+if(name == 'ECMAScript' | name == 'ecmascript'){
+    alert('Right!');
+}
+else {
+alert('Didn’t know? ECMAScript!');
+}
